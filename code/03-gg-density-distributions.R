fc1990b73bfd1b5a8e1ced6836fb022db14cca54
Quinn Webber <webber.quinn@gmail.com>
2023-02-10 09:50:12 -0500

    Update 03-gg-density-distributions.R

--- code/03-gg-density-distributions.R
+++ code/03-gg-density-distributions.R
@@ -18,23 +18,26 @@ setDT(penguins)
 col = c("#1b9e77", "#d95f02", "#7570b3") 
 
 #### assign theme ####
-theme_penguin <- theme(legend.position = 'none',
-                    panel.grid.minor = element_blank(),
-                    panel.background = element_blank(), 
-                    panel.border = element_rect(colour = "black", fill=NA, size = 1))
+theme_penguin <- theme(legend.position = c(0.9, 0.8),
+                       legend.key = element_blank(),
+                       axis.text = element_text(color = "black"),
+                       panel.grid.minor = element_blank(),
+                       panel.background = element_blank(), 
+                       panel.border = element_rect(colour = "black", 
+                                                   fill=NA, size = 1))
 
 
 #### basic density distribution ####
 ggplot(penguins) +
   geom_density(aes(bill_length_mm, 
                    fill = species), 
-               alpha = 0.5) +
-  theme_penguin
+               alpha = 0.5) 
 
 #### update plot 1 ####
 ## add vertical lines for the mean of each distribution
 
-mean_penguin <- setDT(penguins)[, mean(bill_length_mm, na.rm = T), by = "species"]
+mean_penguin <- setDT(penguins)[, mean(bill_length_mm, na.rm = T), 
+                                by = "species"]
 
 ## plot
 ggplot(penguins) +
@@ -50,67 +53,22 @@ ggplot(penguins) +
 ## separate plots (using facet)
 
 ## plot
+png("figures/fig3.png", width = 4000, height = 3000, units = "px", res = 500)
 ggplot(penguins) +
   geom_density(aes(bill_length_mm, 
                    fill = species), 
                alpha = 0.5) +
-  geom_vline(data = mean_penguin, 
-             aes(xintercept = V1), 
-             lty = 2) +
+  scale_fill_manual(values = col) +
+  ylab("Density distribution") +
+  xlab("Bill length (mm)") +
   theme_penguin +
   theme(strip.background = element_rect(color = "black", 
                                         fill = "white", 
                                         size = 1),
         strip.text = element_text(size = 14, color = "black")) +
-  facet_wrap(~species)
-
-#### update plot 3 ####
-## separate plots (without using facet)
-
-## plot
-gg_adelie <- ggplot(penguins[species == "Adelie"]) +
-  geom_density(aes(bill_length_mm, 
-                   fill = species), 
-               alpha = 0.5, 
-               fill = "#1b9e77") +
-  geom_vline(data = mean_penguin[species == "Adelie"], 
-             aes(xintercept = V1), 
-             lty = 2) +
-  ggtitle("A)") +
-  theme_penguin
-  
-gg_chinstrap <- ggplot(penguins[species == "Chinstrap"]) +
-  geom_density(aes(bill_length_mm, 
-                   fill = species), 
-               alpha = 0.5,
-               fill = "#d95f02") +
-  geom_vline(data = mean_penguin[species == "Chinstrap"], 
-             aes(xintercept = V1), 
-             lty = 2) +
-  ggtitle("B)") +
-  theme_penguin
-
-gg_gentoo <- ggplot(penguins[species == "Gentoo"]) +
-  geom_density(aes(bill_length_mm, 
-                   fill = species), 
-               alpha = 0.5,
-               fill = "#7570b3") +
-  geom_vline(data = mean_penguin[species == "Gentoo"], 
-             aes(xintercept = V1), 
-             lty = 2) +
-  ggtitle("C)") +
-  theme_penguin
-
-
-png("figures/fig3.png", width = 3000, height = 2000, units = "px", res = 500)
-gridExtra::grid.arrange(gg_adelie, 
-             gg_chinstrap, 
-             gg_gentoo, 
-             nrow = 1,
-             ncol = 3)
+  facet_wrap(~island)
 dev.off()
-
-
+  
 #### ggridges ####
 
 data("lincoln_weather")
